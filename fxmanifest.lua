8560c5adf783063897613cf7f077a8ad2f5042f6
Leon Maximus <ritcheimmanuelb@gmail.com>
2022-12-08 06:34:20 +0800

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+description 'Leon Maximus HomeScoreboard'
+version '1.0'
+
+server_script 'server/main.lua'
+client_script 'client/*.lua'
+
+ui_page 'html/index.html'
+
+files {
+	'html/*'
+}
