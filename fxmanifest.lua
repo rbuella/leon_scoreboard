742167906c4467dbdd851b91cdb621f1ce979de7
Leon Maximus <ritcheimmanuelb@gmail.com>
2025-07-17 13:58:20 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ fx_version 'adamant'
 game 'gta5'
 description 'Leon Maximus HomeScoreboard'
 version '1.0'
+lua54 'yes'
 
 server_script 'server/main.lua'
 client_script 'client/*.lua'
@@ -12,3 +13,11 @@ ui_page 'html/index.html'
 files {
 	'html/*'
 }
+
+escrow_ignore {
+  "html/*",
+  "server/*",
+}
+
+ 
+dependency '/assetpacks'
